8b093201056048bc4dbdf776e12571c7cbcde8d5
Raphael Silva Ramalho <raphaelsilvaramalho.com@gmail.com>
2024-05-08 10:23:01 -0300

    tabela

--- src/comps/FoodTable.jsx
+++ src/comps/FoodTable.jsx
@@ -4,38 +4,78 @@ const FoodTable = ({ mealsData }) => {
   return (
     <>
       {mealsData ? (
-        <div>
-          <div>
-            <h2>
-              Café da manhã:{" "}
-              {
-                mealsData.breakfast[mealsData.breakfast.length - 1]
-                  .total_calories
-              }{" "}
-              kcal
-            </h2>
-          </div>
-          <div>
-            <h2>
-              Almoço:{" "}
-              {mealsData.lunch[mealsData.lunch.length - 1].total_calories} kcal
-            </h2>
-          </div>
-          <div>
-            <h2>
-              Lanche da tarde:
-              {
-                mealsData.snacks[mealsData.snacks.length - 1].total_calories
-              }{" "}
-              kcal
-            </h2>
-          </div>
-          <div>
-            <h2>
-              Jantar{" "}
-              {mealsData.dinner[mealsData.dinner.length - 1].total_calories}{" "}
-              kcal
-            </h2>
+        <div className="my-20 text-xl">
+          <p className="text-4xl font-bold mb-5 sticky top-0 mx-0 bg-white py-10 border-b-4 px-10">
+            Calorias Totais do dia: {mealsData.total_calories} Kcal
+          </p>
+          <div className="mx-10 grid grid-cols-1 gap-10 mt-10">
+            <div className="w-1/2">
+              <h2 className="text-3xl mb-5 font-semibold border-b-4 pb-5">
+                Café da manhã:{" "}
+                {
+                  mealsData.breakfast[mealsData.breakfast.length - 1]
+                    .total_calories
+                }{" "}
+                kcal
+              </h2>
+              {mealsData.breakfast.map((item) => {
+                if (item.item != null)
+                  return (
+                    <div key={item.item} className="flex gap-10">
+                      <p className="font-medium">{item.calories} Kcal</p>
+                      <p>{item.item}</p>
+                    </div>
+                  );
+              })}
+            </div>
+            <div className="w-1/2">
+              <h2 className="text-3xl mb-5 font-semibold border-b-4 pb-5">
+                Almoço:{" "}
+                {mealsData.lunch[mealsData.lunch.length - 1].total_calories}{" "}
+                kcal
+              </h2>
+              {mealsData.lunch.map((item) => {
+                if (item.item != null)
+                  return (
+                    <div key={item.item} className="flex gap-10">
+                      <p className="font-medium">{item.calories} Kcal</p>
+                      <p>{item.item}</p>
+                    </div>
+                  );
+              })}
+            </div>
+            <div className="w-1/2">
+              <h2 className="text-3xl mb-5 font-semibold border-b-4 pb-5">
+                Café da tarde:{" "}
+                {mealsData.snacks[mealsData.snacks.length - 1].total_calories}{" "}
+                kcal
+              </h2>
+              {mealsData.snacks.map((item) => {
+                if (item.item != null)
+                  return (
+                    <div key={item.item} className="flex gap-10">
+                      <p className="font-medium">{item.calories} Kcal</p>
+                      <p>{item.item}</p>
+                    </div>
+                  );
+              })}
+            </div>
+            <div className="w-1/2">
+              <h2 className="text-3xl mb-5 font-semibold border-b-4 pb-5">
+                Jantar:{" "}
+                {mealsData.dinner[mealsData.dinner.length - 1].total_calories}{" "}
+                kcal
+              </h2>
+              {mealsData.dinner.map((item) => {
+                if (item.item != null)
+                  return (
+                    <div key={item.item} className="flex gap-10">
+                      <p className="font-medium">{item.calories} Kcal</p>
+                      <p>{item.item}</p>
+                    </div>
+                  );
+              })}
+            </div>
           </div>
         </div>
       ) : null}
--- src/comps/LoginModal.jsx
+++ src/comps/LoginModal.jsx
@@ -13,29 +13,31 @@ const LoginModal = ({ username, setUsername, setModalShow, onConfirmBtn }) => {
   return (
     <>
       {!loading ? (
-        <div className="p-10 flex fixed flex-col bg-red-700 items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <p className="text-2xl">
+        <div className="p-10 flex fixed flex-col bg-gray-300 rounded-xl items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+          <p className="text-3xl mb-10 text-center font-bold">
             Primeiro digite seu nome de usuário no MyfitnessPal:
           </p>
           <input
             type="text"
             value={username}
             onChange={(event) => setUsername(event.target.value)}
-            className="border-2 border-black text-xl rounded-md px-2 w-5/12"
+            className="border-2 border-black text-2xl rounded-md px-2 w-8/12 "
           />
           <button
             onClick={() => handleClick()}
-            className="mt-10 place-self-center border-2 border-black px-24 py-4 rounded-full font-bold text-2xl text-center"
+            className="mt-10 place-self-center border-2 border-black px-24 py-4 rounded-full font-bold text-2xl text-center hover:bg-slate-500 duration-200 hover:scale-105"
           >
             Buscar
           </button>
         </div>
       ) : (
-        <div className="p-10 flex fixed flex-col bg-red-700 items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <p>
-            Buscando dados de <strong>{username}</strong>
+        <div className="p-10 flex fixed flex-col bg-gray-300 rounded-xl items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+          <p className="text-2xl">
+            Buscando dados de <strong className="text-3xl">{username}</strong>
           </p>
-          <p>(Cheque se o nome digitado existe no banco do MyfitnessPal)</p>
+          <p className="text-xl mt-5">
+            (Cheque se o nome digitado existe no banco do MyfitnessPal)
+          </p>
         </div>
       )}
     </>
