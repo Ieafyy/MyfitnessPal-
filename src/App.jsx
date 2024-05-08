a66ef31da32eab360f9a57eb62b84a19e56524bb
Raphael Silva Ramalho <raphaelsilvaramalho.com@gmail.com>
2024-05-08 10:23:25 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import FoodTable from "./comps/FoodTable";
 import LoginModal from "./comps/LoginModal";
 function App() {
